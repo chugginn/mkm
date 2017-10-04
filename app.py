bf60d8f4bcf6a03b3c4aa952cd6ef905f12b4ec9
Mark Ramos <markramos83@gmail.com>
2017-10-04 13:35:20 -0500

    Demo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-
+from os import listdir
+from os.path import isfile, join
 
 from flask import Flask
 from flask import render_template
@@ -8,7 +9,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    with open('yt/yt.txt') as f:
+        videos = f.readlines()
+        videos = [x.strip() for x in videos]
+    return render_template('index.html', videos=videos)
+
+
+@app.route('/shows')
+def shows():
+    return render_template('shows.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/photos')
+def photos():
+    photo_list = [f for f in listdir('/static/img/gallery') if isfile(join('/static/img/gallery', f))]
+    return render_template('photos.html', photo_list=sorted(photo_list, reverse=True))
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 app.run(debug=True)
