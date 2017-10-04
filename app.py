32815ee5e12ac188f86753319f83e65068037804
Mark Ramos <markramos83@gmail.com>
2017-10-04 13:31:11 -0500

    Demo

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from os import listdir
-from os.path import isfile, join
+
 
 from flask import Flask
 from flask import render_template
@@ -10,26 +9,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/shows')
-def shows():
-    return render_template('shows.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/photos')
-def photos():
-    return render_template('photos.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 
 app.run(debug=True)
